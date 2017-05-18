4bcb85ac6154d86c120f18f5fd022dad6cef3975
Harry Caufield <j.harry.caufield@gmail.com>
2017-05-18 10:14:16 -0700

    Package setup

---
+++ heartcases/__init__.py
@@ -0,0 +1,6 @@
+'''
+heartCases is a medical language processing system for case reports 
+involving cardiovascular disease (CVD).
+'''
+__author__= "Harry Caufield"
+__email__ = "j.harry.caufield@gmail.com"
---
+++ heartcases/heartCases_learn.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#heartCases_learn.py
+'''
+heartCases is a medical language processing system for case reports 
+involving cardiovascular disease (CVD).
+
+This part of the system is intended for learning from the labels and
+features identified in a set of case reports processed by
+heartCases_read.
+
+Requires bokeh, numpy, nltk, and sklearn.
+
+'''
+__author__= "Harry Caufield"
+__email__ = "j.harry.caufield@gmail.com"
+
+import sys
+
+#Constants and Options
+
+#Functions
+
+#Main
+def main():
+	pass
+
+if __name__ == "__main__":
+	sys.exit(main())
